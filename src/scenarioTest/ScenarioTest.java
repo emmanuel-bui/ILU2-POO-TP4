708e1228ddba1b86653592a7fd2fbd0dd50adaa7
emmanuel-bui <manuche@tuta.io>
2025-11-20 08:18:16 +0100

    Revert "tt cassé"
    This reverts commit 696c5ab891cb07652cdcb064f057d6e343f03b

--- src/scenarioTest/ScenarioTest.java
+++ src/scenarioTest/ScenarioTest.java
@@ -1,10 +1,6 @@
 package scenarioTest;
 
-import personnages.Gaulois;
-import produit.Poisson;
-import produit.Sanglier;
 import villagegaulois.Etal;
-import villagegaulois.IEtal;
 
 public class ScenarioTest {
 
@@ -35,10 +31,7 @@ public class ScenarioTest {
 	}
 
 	public static void main(String[] args) {
-		IEtal[] etals = new IEtal[3];
-		Etal<Sanglier> etalSanglier = new Etal<>();
-		etals[0] = etalSanglier;
-		etals[0].occuperEtal(new Gaulois("Ordralfabétix", 12), new Poisson("lundi"), 10);
+
 	}
 
 }
